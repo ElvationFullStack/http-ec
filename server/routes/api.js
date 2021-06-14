c28f7826828a6f8a92bf30959042555da4547130
unknown <mosa-rko@windowslive.com>
2021-06-14 11:43:43 +0300

    http basic request

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,35 +1,86 @@
 const express = require('express')
 const router = express.Router()
 const empty_array = []
-let video_complete_counter = 0 // /we will do it three part
 const adminArray = ["Yones"]
-const searchArray=["google","apple"]
-//=============Informational responses 100-199 =================
+const searchArray = ["google", "apple"]
+//=======================Client errors (400–499)  ========================
 
-//==============ask Lotem or Lior ============
-router.post('/100', function (req, res) {
-    console.log("----------", req.headers.expect)
-    if (req.headers.expect === "100-continue") {
-        console.log("__________________________100")
-        if (video_complete_counter >= 3) {
-            res.send(" video is complete ")
-        } else {
-
-            video_complete_counter++
-            console.log("counter ", video_complete_counter)
-
-            res.status(101)
-            res.send('send rest')
-
-        }
+router.get('/getData/:admin', function (req, res) {
+    console.log(" ------------admins ", adminArray)
+    const admin = req.params.admin
+    //================need admin authority======================
+    if (!adminArray.includes(admin)) {
+        res.statusCode = 401;// we detrmin what status is 
+        res.send('no accsess right')
     }
 
+    res.send(` user ${admin} has accsess right her are your users 1 2 3 `)// this will not reach 
+})
+router.post('/addAdmin/:admin', function (req, res) {// 201 
+    const NewAdmin = req.params.admin
+    res.status(201)
+    adminArray.push(NewAdmin)
+    res.send(`congrats ${NewAdmin} you are now an admin`)
+})
+//======================Server errors (500)->(599) ==================
+router.get('/getDataFromArray', function (req, res) {
+    //=========some error in the server===========
+    const arr = undefined
+    let item = arr[0];
+    //=====================================================
+    res.sent(item)
+})
 
+
+///===================dho eror 404 not found ================================
+
+
+
+//================================homeWork==========================
+// try to read the url 
+router.get('/clientError1', function (req, res) {
+    const url = "http://thisUrlIsSoDamnLongForMeToParsePleaseGivmeSomtingLess"
+
+})
+//this route can handle max 5 request 
+//what should you do if cliend send yo more than 5 request ?
+router.get('/clientError2/:requstNumber', function (req, res) {
 
 })
 
-//No, the WebSocket clients sends the HTTP request asking for a WebSocket connection, then the server responds with an HTTP 101 Switching protocols, meaning that it accepts the connection, and then the client can start to send and receive data in binary format.
+////////////////////you are trying to acsess the next url ////
+const someUrl = "changedForKnow.com"
+router.get('/redirectError1', function (req, res) {
+    ///the rquste change has benn temporarily  check after two hour 
 
+})
+
+const someUrl2 = "changedForAlways.com"
+
+router.get('/redirectError2', function (req, res) {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==========================================================================================================
+//==========================================================================================================
+//======================================Optional some routes i added  ======================================
+//No, the WebSocket clients sends the HTTP request asking for a WebSocket connection, then the server responds with an HTTP 101 Switching protocols, meaning that it accepts the connection, and then the client can start to send and receive data in binary format.
 // first we inclode in headers Upradge =websocket
 // then we check without header 
 router.get('/101', function (req, res) {
@@ -40,7 +91,6 @@ router.get('/101', function (req, res) {
     }
     res.send(' accept portocol change ')
 })
-
 //=========================Successful responses (200–299)  ==========================
 router.get('/204', function (req, res) {// response no content
     if (empty_array.length === 0) {
@@ -49,95 +99,22 @@ router.get('/204', function (req, res) {// response no content
     res.send('affter adding somthing you have content ')// this will not be send 
 
 })
-
-router.post('/204', function (req, res) {
-    empty_array.push("NewItemAdde");
-    res.send("added successfuly ")
-})
-
-
-
-
 //=======================Redirects (300–399)==========================
-// go to http://example.com/ 
 const redirect = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods"
 router.get('/303/:http', function (req, res) {// refer to othe  
     const search = req.params.http
-if(!searchArray.includes(search)){
-    res.status(303)
-    res.send(redirect)
-}
-let str="<div><ul>"
-const send=()=>{searchArray.forEach(e=>{ str +=`<li><a href=" link to ">${e}</a></li>`})}
-send()
-str+='</ul></div>'
-console.log(str)
-res.send(`${str}`)
-
-})
-
-
-//=======================Client errors (400–499)  ========================
-
-router.get('/403/:admin', function (req, res) {
-    console.log(" ------------admins ", adminArray)
-    const admin = req.params.admin
-    //================need admin authority======================
-    if (!adminArray.includes(admin)) {
-        res.statusCode = 403;// we detrmin what status is 
-        res.send('no accsess right')
+    if (!searchArray.includes(search)) {
+        res.status(303)
+        res.send(redirect)
     }
-
-    res.send(` user ${admin} has accsess right`)// this will not reach 
-})
-router.post('/403/:admin', function (req, res) {
-    const NewAdmin = req.params.admin
-    adminArray.push(NewAdmin)
-    res.send(`congrats ${NewAdmin} you are now an admin`)
-})
-
-
-//======================Server errors (500)->(599) ==================
-
-
-router.get('/500', function (req, res) {
-    //=========some error in the server===========
-    const arr = undefined
-    arr[0];
-    //=====================================================
+    let str = "<div><ul>"
+    const send = () => { searchArray.forEach(e => { str += `<li><a href=" link to ">${e}</a></li>` }) }
+    send()
+    str += '</ul></div>'
+    console.log(str)
+    res.send(`${str}`)
 
 })
-
-//================================homeWork==========================
-
-
-
-
-// try to read the url 
-router.get('/clientError1',function(req,res){
-    const url="http://thisUrlIsSoDamnLongForMeToParsePleaseGivmeSomtingLess"
-
-})
-//this route can handle max 5 request 
-//what should you do if cliend send yo more than 5 request ?
-router.get('/clientError2/:requstNumber',function(req,res){
-})
-
-////////////////////you are trying to acsess the next url ////
-const someUrl="changedForKnow.com"
-router.get('/redirectError1',function(req,res){
-    ///the rquste change has benn temporarily  check after two hour 
-    
-})
-
-const someUrl2="changedForAlways.com"
-
-router.get('/redirectError2',function(req,res){
-    
-})
-
-
-
 
 
 
